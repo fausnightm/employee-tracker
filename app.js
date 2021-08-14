88b3e97496dd6bb9fcf0d255c522db9283fdd70e
Fausnightm <m.fausnight@lafilm.edu>
2021-08-14 11:56:20 -0400

    schema added

---
+++ app.js
@@ -0,0 +1,12 @@
+// dependencies
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+require("console.table");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "Password",
+    database: "employeesDB"
+  });
